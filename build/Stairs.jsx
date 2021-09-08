f2ac5f961b5d5731c5927397831258ea0ff32078
Tom Genoni <tom@genoni.dev>
2021-09-08 16:00:25 -0700

    Seed

---
+++ build/Stairs.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Icon = () => {
+  return (
+ <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.66669 13.3333H5.33335V10.6666H8.00002V7.99998H10.6667V5.33331H13.3334" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2.66669 7.33335L7.33335 2.66669M7.33335 2.66669V5.33335M7.33335 2.66669H4.66669" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+ 
+  );
+}
+
+export default Icon;
